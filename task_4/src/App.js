1675ba6f1a11fbff9ba45d9cb6987e18112829d7
Denis Chuvakov <ds.chuvakov@gmail.com>
2023-05-14 20:19:30 +0300

    Add files via upload

---
+++ task_4/src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./App.css";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import CustomerList from "./components/CustomerList";
+import TrainingList from "./components/TrainingList";
+import MyCalendar from "./components/MyCalendar";
+import NotFound from "./components/NotFound";
+import Chart from "./components/Chart";
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <AppBar position="static">
+          <Toolbar style={{ flexGrow: 1 }}>
+            <Typography variant="h6">PersonalTrainer</Typography>
+
+            <Typography
+              variant="h6"
+              className="App-bar"
+              style={{ marginLeft: "auto" }}
+            >
+              <Link to="/">Customers</Link>
+            </Typography>
+            <Typography variant="h6" className="App-bar">
+              <Link to="/trainings">Trainings</Link>
+            </Typography>
+            <Typography variant="h6" className="App-bar">
+              <Link to="/calendar">Calendar</Link>
+            </Typography>
+            <Typography variant="h6" className="App-bar">
+              <Link to="/chart">Chart</Link>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Routes>
+          <Route path="/" element={<CustomerList />} />
+          <Route path="/trainings" element={<TrainingList />} />
+          <Route path="/calendar" element={<MyCalendar />} />
+          <Route path="/chart" element={<Chart />} />
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
